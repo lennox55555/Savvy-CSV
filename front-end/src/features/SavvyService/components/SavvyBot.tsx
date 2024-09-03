33103e3e774f166eeceee95227b11903f3f410f0
Lennox Anderson <60410047+lennox55555@users.noreply.github.com>
2024-09-03 12:32:41 -0400

    Finished WebSocket and Construct Table

--- front-end/src/features/SavvyService/components/SavvyBot.tsx
+++ front-end/src/features/SavvyService/components/SavvyBot.tsx
@@ -1,17 +1,15 @@
+import React, { useState, useEffect } from 'react';
 import styles from './SavvyBot.module.css';
-import './SavvyTable'
-import SavvyTable from './SavvyTable';
 import AutosizeTextArea from '../../../utils/useAutosizeTextArea';
-import { useEffect, useState } from 'react';
-import SavvyServiceAPI from '../../../services/savvyServiceAPI';
 import { getAuth } from 'firebase/auth';
+import SavvyServiceAPI from '../../../services/savvyServiceAPI';
 import { Link } from 'react-router-dom';
 
 const SavvyBot: React.FC = () => {
-
     const [textAreaValue, setTextAreaValue] = useState('');
     const [messages, setMessages] = useState<{ id: string; text: string; user: boolean }[]>([]);
     const [isUser, setIsUser] = useState(true);
+    const [tableData, setTableData] = useState<NestedObject | null>(null);
 
     const fetchMessages = async () => {
         const currentUser = getAuth().currentUser;
@@ -24,11 +22,11 @@ const SavvyBot: React.FC = () => {
                 if (err instanceof Error) {
                     console.log(err.message);
                 } else {
-                    console.log('An error has occured!');
+                    console.log('An error has occurred!');
                 }
             }
         }
-    }
+    };
 
     const handleSubmit = async () => {
         if (textAreaValue.trim() !== '') {
@@ -39,47 +37,54 @@ const SavvyBot: React.FC = () => {
                     await SavvyServiceAPI.getInstance().saveMessage(currentUser.uid, textAreaValue, isUser);
 
                     setMessages([...messages, { id: '', text: textAreaValue, user: isUser }]);
-
                     setTextAreaValue('');
                     setIsUser(prevState => !prevState);
                     fetchMessages();
+
+                    // Initialize WebSocket and listen for data
+                    SavvyServiceAPI.getInstance().initializeWebSocket(handleWebSocketMessage);
+
                 } catch (error) {
                     console.error("Failed to save message:", error);
                 }
             }
         }
-    }
+    };
+
+    const handleWebSocketMessage = (data: any) => {
+        setTableData(data);
+    };
 
     const handleKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
         if (event.key === 'Enter' && !event.shiftKey) {
             event.preventDefault();
             handleSubmit();
         }
-    }
-    //Web socket code
+    };
 
-// Build table
-    function displayCSVAsTable(csv: string): void {
-        const rows = csv.split('\n');
-
-        const table = rows.map(row => row.split(','));
-
-        const columnWidths = table[0].map((_, i) => Math.max(...table.map(row => row[i].length)));
-
-        table.forEach(row => {
-            const formattedRow = row.map((value, i) => value.padEnd(columnWidths[i]));
-            console.log(formattedRow.join(' | '));
-        });
-    }
-
-
-    const csvData = `Name, Age, Occupation
-    John Doe, 30, Software Engineer
-    Jane Smith, 25, Data Analyst
-    Sam Johnson, 40, Project Manager`;
-
-    displayCSVAsTable(csvData);
-
+    const displayTableForRank1 = (data: NestedObject): JSX.Element | null => {
+        for (const key in data) {
+            if (data[key].rankOfTable === 1) {
+                return (
+                    <div>
+                        <h3>Website: <a href={data[key].website} target="_blank" rel="noopener noreferrer">{data[key].website}</a></h3>
+                        <table className={styles.csvTable}>
+                            <tbody>
+                            {data[key].SampleTableData.split('\n').map((row, index) => (
+                                <tr key={index}>
+                                    {row.split(',').map((cell, cellIndex) => (
+                                        <td key={cellIndex} className={styles.customTableCell}>{cell.trim()}</td>
+                                    ))}
+                                </tr>
+                            ))}
+                            </tbody>
+                        </table>
+                    </div>
+                );
+            }
+        }
+        return null;
+    };
 
     useEffect(() => {
         fetchMessages();
@@ -89,43 +94,17 @@ const SavvyBot: React.FC = () => {
         <>
             <div className={styles.savvybotContainer}>
                 <div className={styles.messageBox}>
-                    <div className={styles.savvyResponse}>
-                        <div className={styles.messageBubble}>
-                            Hello there!
-                        </div>
-                    </div>
-                    <div className={styles.userMessage}>
-                        <div className={styles.messageBubble}>
-                            Show me NVIDIA's daily stock price in 2024
-                        </div>
-                    </div>
-                    <div className={styles.savvyResponse}>
-                        <div className={styles.messageBubble}>
-                            Here's a table of NVIDIA's daily stock price performance in 2024.
-                        </div>
-                    </div>
-                    <div className={styles.savvyResponse}>
-                        <SavvyTable />
-                    </div>
-                    <div className={styles.userMessage}>
-                        <div className={styles.messageBubble}>
-                            Show me NVIDIA's daily stock price in 2024
-                        </div>
-                    </div>
-                    <div className={styles.savvyResponse}>
-                        <div className={styles.messageBubble}>
-                            Here's a table of NVIDIA's daily stock price performance in 2024.
-                        </div>
-                    </div>
-                    <div className={styles.savvyResponse}>
-                        <SavvyTable />
-                    </div>
-                    {/* Add more messages here as needed */}
                     {messages.map((msg, index) => (
                         <div key={index} className={msg.user ? styles.userMessage : styles.savvyResponse}>
-                        <div className={styles.messageBubble}>{msg.text}</div>
-                    </div>
+                            <div className={styles.messageBubble}>{msg.text}</div>
+                        </div>
                     ))}
+
+                    {tableData && (
+                        <div className={styles.savvyResponse}>
+                            {displayTableForRank1(tableData)}
+                        </div>
+                    )}
                 </div>
                 <div className={styles.messageBar}>
                     <div className={styles.messageBarWrapper}>
@@ -149,4 +128,4 @@ const SavvyBot: React.FC = () => {
     );
 }
 
-export default SavvyBot
+export default SavvyBot;
--- front-end/src/services/savvyServiceAPI.ts
+++ front-end/src/services/savvyServiceAPI.ts
@@ -1,24 +1,21 @@
 import { addDoc, collection, doc, getDocs, getFirestore, orderBy, query } from "firebase/firestore";
-import UserServiceAPI from "./userServiceAPI"
 import { db } from "../firebase/firebase-init";
-
 
 class SavvyServiceAPI {
     private static instance: SavvyServiceAPI;
+    private webSocket: WebSocket | null = null;
+    private objec: string = ""; // To accumulate WebSocket messages
 
     private constructor() { }
 
     public static getInstance(): SavvyServiceAPI {
-
         if (!SavvyServiceAPI.instance) {
             SavvyServiceAPI.instance = new SavvyServiceAPI();
         }
-
         return SavvyServiceAPI.instance;
     }
 
     public async saveMessage(userId: string, message: string, sentByUser: boolean) {
-
         try {
             const userMessageRef = collection(doc(db, 'users', userId), 'messages');
             const messageDoc = await addDoc(userMessageRef, {
@@ -26,7 +23,7 @@ class SavvyServiceAPI {
                 user: sentByUser,
                 timestamp: new Date(),
             });
-            console.log('Message saved:', messageDoc.id)
+            console.log('Message saved:', messageDoc.id);
         } catch (error) {
             console.error("Error saving message:", error);
             throw error;
@@ -34,7 +31,6 @@ class SavvyServiceAPI {
     }
 
     public async getMessages(userId: string) {
-
         try {
             const userMessagesRef = collection(doc(db, "users", userId), "messages");
             const q = query(userMessagesRef, orderBy("timestamp"));
@@ -44,7 +40,7 @@ class SavvyServiceAPI {
                 const data = doc.data();
                 return {
                     id: doc.id,
-                    text: data.text || '', 
+                    text: data.text || '',
                     user: data.user || false,
                 };
             });
@@ -54,7 +50,63 @@ class SavvyServiceAPI {
             console.error("Error getting messages:", error);
             throw error;
         }
+    }
 
+    public initializeWebSocket(onMessageReceived: (data: any) => void): void {
+        const wsUrl = 'wss://9f2wyu1469.execute-api.us-east-1.amazonaws.com/production/';
+        const queries = ["What is the average snowfall per city in colorado?"]; // The specific query to send
+        this.webSocket = new WebSocket(wsUrl);
+
+        this.webSocket.onopen = () => {
+            console.log('WebSocket connection established');
+            this.sendQueries(queries);
+        };
+
+        this.webSocket.onmessage = (event: MessageEvent) => {
+            console.log(event.data);
+            this.handleMessage(event.data, onMessageReceived);
+        };
+
+        this.webSocket.onerror = (event: Event) => {
+            console.error('WebSocket error observed:', event);
+        };
+
+        this.webSocket.onclose = (event: CloseEvent) => {
+            console.log('WebSocket connection closed:', event);
+        };
+    }
+
+    private sendQueries(queries: string[]): void {
+        if (this.webSocket && this.webSocket.readyState === WebSocket.OPEN) {
+            const message = { action: "sendQueries", columnNames: queries };
+            this.webSocket.send(JSON.stringify(message));
+            console.log('Queries sent to the server:', queries);
+        } else {
+            console.error('WebSocket is not open. Cannot send queries.');
+        }
+    }
+
+    private handleMessage(data: string, onMessageReceived: (data: any) => void): void {
+        this.objec += data;
+
+        let openBrackets = 0;
+        let closeBrackets = 0;
+
+        for (const char of this.objec) {
+            if (char === '{') openBrackets++;
+            if (char === '}') closeBrackets++;
+        }
+
+        if (openBrackets > 0 && openBrackets === closeBrackets) {
+            try {
+                const fullObject = JSON.parse(this.objec);
+                onMessageReceived(fullObject); // Pass the full object to the callback
+                this.objec = ""; // Reset the accumulated string
+            } catch (error) {
+                console.error('Error processing the complete message:', error);
+                this.objec = "";
+            }
+        }
     }
 }
 
